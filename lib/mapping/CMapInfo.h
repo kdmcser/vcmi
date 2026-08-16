264ac6538af2856a38ad8e135698e4557f173e6a
kdmcser <zqtndfj@sina.com>
2026-08-17 01:50:27 +0800

    Load map file metadata lazily to avoid filesystem I/O on cache load

--- lib/mapping/CMapInfo.h
+++ lib/mapping/CMapInfo.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "../serializer/Serializeable.h"
+#include "../filesystem/ResourcePath.h"
 
 VCMI_LIB_NAMESPACE_BEGIN
 
@@ -17,7 +18,6 @@ struct StartInfo;
 
 class CMapHeader;
 class Campaign;
-class ResourcePath;
 class BinaryDeserializer;
 
 /**
@@ -32,9 +32,6 @@ public:
 	std::unique_ptr<StartInfo> scenarioOptionsOfSave; // Options with which scenario has been started (used only with saved games)
 	std::string fileURI;
 	std::string originalFileURI; // no need to serialize
-	std::string fullFileURI; // no need to serialize
-	std::time_t lastWrite; // no need to serialize
-	std::string date;
 	int amountOfPlayersOnMap;
 	int amountOfHumanControllablePlayers;
 	int amountOfHumanPlayersInSave;
@@ -54,6 +51,7 @@ public:
 	void campaignInit();
 	void countPlayers();
 	void initFromCache(const std::string & fileURI, BinaryDeserializer & h);
+	void initCampaignFromCache(const std::string & fileURI, BinaryDeserializer & h, const std::string & modName);
 	
 	std::string getNameTranslated() const;
 	std::string getNameForList() const;
@@ -62,18 +60,32 @@ public:
 	int getMapSizeFormatIconId() const;
 	std::string getMapSizeName() const;
 
+	const std::string & getFullFileURI() const;
+	std::time_t getLastWrite() const;
+	const std::string & getDate() const;
+
 	template <typename Handler> void serialize(Handler &h)
 	{
 		h & mapHeader;
 		h & campaign;
 		h & scenarioOptionsOfSave;
 		h & fileURI;
+		ensureFileMetadata();
 		h & date;
 		h & amountOfPlayersOnMap;
 		h & amountOfHumanControllablePlayers;
 		h & amountOfHumanPlayersInSave;
 		h & isRandomMap;
 	}
+
+private:
+	void ensureFileMetadata() const;
+
+	ResourcePath fileMetadataResource{ "", EResType::OTHER };
+	mutable bool fileMetadataLoaded = false;
+	mutable std::string fullFileURI;
+	mutable std::time_t lastWrite = 0;
+	mutable std::string date;
 };
 
 VCMI_LIB_NAMESPACE_END
