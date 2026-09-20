03d77ac852c97960601a45e4e9aadd94cf5c2cf1
kdmcser <zqtndfj@sina.com>
2026-09-20 23:33:25 +0800

    Replace targetSourceType selector with targetSource

--- lib/bonuses/BonusSelector.cpp
+++ lib/bonuses/BonusSelector.cpp
@@ -33,12 +33,6 @@ namespace Selector
 		return ssourceType;
 	}
 
-	DLL_LINKAGE const CSelectFieldEqual<BonusSource> & targetSourceType()
-	{
-		static const CSelectFieldEqual<BonusSource> ssourceType(&Bonus::targetSourceType);
-		return ssourceType;
-	}
-
 	DLL_LINKAGE const CSelectFieldEqual<BonusLimitEffect> & effectRange()
 	{
 		static const CSelectFieldEqual<BonusLimitEffect> seffectRange(&Bonus::effectRange);
